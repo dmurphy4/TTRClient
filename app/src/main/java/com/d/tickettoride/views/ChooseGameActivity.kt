c837cf42619de7100dedd375348b14878de0d327
zacharytaylor <zachtylr04@gmail.com>
2019-01-24 15:55:46 -0700

    Create ChooseGame and Lobby activities

---
+++ app/src/main/java/com/d/tickettoride/views/ChooseGameActivity.kt
@@ -0,0 +1,13 @@
+package com.d.tickettoride.views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.d.tickettoride.R
+
+class ChooseGameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_game)
+    }
+}
