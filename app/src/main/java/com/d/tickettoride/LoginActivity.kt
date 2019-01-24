a73568bc464f6dc50181c43fc59f36dd5ef7e5cf
zacharytaylor <zachtylr04@gmail.com>
2019-01-23 21:58:49 -0700

    Create register activity .kt and xml

--- app/src/main/java/com/d/tickettoride/LoginActivity.kt
+++ app/src/main/java/com/d/tickettoride/LoginActivity.kt
@@ -1,12 +1,17 @@
 package com.d.tickettoride
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        button_register.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
