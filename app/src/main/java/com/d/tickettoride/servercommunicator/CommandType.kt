693578d1e33040e53d201a973fa528cf2169ed37
dmurphy4 <murphy.dallin@byu.edu>
2019-02-08 12:35:26 -0700

    Hey Zach, good to hear from you recently. This is my commit message. I made the command data class

--- app/src/main/java/com/d/tickettoride/servercommunicator/CommandType.kt
+++ app/src/main/java/com/d/tickettoride/servercommunicator/CommandType.kt
@@ -1,6 +1,6 @@
 package com.d.tickettoride.servercommunicator
 
 enum class CommandType {
-    C_BEGIN_PLAY, C_CREATE_GAME, C_JOIN_GAME, C_LOGIN, C_POLL,
+    C_BEGIN_PLAY, C_CREATE_GAME, C_JOIN_GAME, C_LOGIN, C_POLL, C_REMOVE_GAME,
     S_CREATE_GAME, S_JOIN_GAME, S_LOGIN, S_REGISTER, S_POLL
 }
