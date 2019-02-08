163f9e6304e06c8a2183a773227efa950476c418
zacharytaylor <zachtylr04@gmail.com>
2019-02-08 14:34:56 -0700

    Minor change to command data class

--- app/src/main/java/com/d/tickettoride/servercommunicator/CommandData.kt
+++ app/src/main/java/com/d/tickettoride/servercommunicator/CommandData.kt
@@ -5,15 +5,15 @@ import com.d.tickettoride.command.client.CCreateGameCommand
 import com.d.tickettoride.command.client.CRemoveGameCommand
 import com.google.gson.Gson
 
-class CommandData(var type:CommandType, var data:String) {
+class CommandData(var type:String, var data:String) {
 
     fun execute(){
-        when (type) {
+        when (CommandType.valueOf(type)) {
             CommandType.C_CREATE_GAME -> Gson().fromJson(data, CCreateGameCommand::class.java).execute()
             CommandType.C_BEGIN_PLAY -> Gson().fromJson(data, CBeginPlayCommand::class.java).execute()
             CommandType.C_REMOVE_GAME -> Gson().fromJson(data, CRemoveGameCommand::class.java).execute()
 
-            else -> println("Leilani Fambuena")
+            else -> println("Leilani Fonbuena")
         }
 
     }
