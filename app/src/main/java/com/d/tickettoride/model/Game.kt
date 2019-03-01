b8a8b50af77d905ea470d6796da394744c1c1c09
dmurphy4 <murphy.dallin@byu.edu>
2019-03-01 14:06:26 -0700

    game

--- app/src/main/java/com/d/tickettoride/model/Game.kt
+++ app/src/main/java/com/d/tickettoride/model/Game.kt
@@ -3,6 +3,5 @@ package com.d.tickettoride.model
 import com.d.tickettoride.model.gameplay.Board
 import com.d.tickettoride.model.gameplay.Event
 
-class Game(val gameInfo: GameInfo, var gamePlayers:ArrayList<PlayerInfo>, var board: Board, var eventHistory:List<Event>) {
-
-}
+data class Game(val gameInfo: GameInfo, var gamePlayers:List<PlayerInfo>, var board: Board,
+           var playerTurnOrder:List<Int>, var eventHistory:List<Event>)
