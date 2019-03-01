2a37a3201075eedf82781825163c94037712c886
dmurphy4 <murphy.dallin@byu.edu>
2019-03-01 11:10:57 -0700

    Hands

--- app/src/main/java/com/d/tickettoride/model/User.kt
+++ app/src/main/java/com/d/tickettoride/model/User.kt
@@ -1,3 +1,7 @@
 package com.d.tickettoride.model
 
-data class User(var userName:String, var playerInfo:PlayerInfo)
+import com.d.tickettoride.model.gameplay.DestinationCardHand
+import com.d.tickettoride.model.gameplay.TrainCarHand
+
+data class User(var userName:String, var playerInfo:PlayerInfo?,
+                var trainHand: TrainCarHand?, var destinationHand:DestinationCardHand?)
---
+++ app/src/main/java/com/d/tickettoride/model/gameplay/TrainCarHand.kt
@@ -0,0 +1,15 @@
+package com.d.tickettoride.model.gameplay
+
+class TrainCarHand : IHand {
+    override fun sort() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun draw(cardsDrawn: List<ICard>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun playCards() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
