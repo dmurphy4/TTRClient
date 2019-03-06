ede90869aeeac436b6e76b60df79a01c7d607457
dmurphy4 <murphy.dallin@byu.edu>
2019-03-06 10:15:16 -0700

    chat presenter

--- app/src/main/java/com/d/tickettoride/model/User.kt
+++ app/src/main/java/com/d/tickettoride/model/User.kt
@@ -3,7 +3,7 @@ package com.d.tickettoride.model
 import com.d.tickettoride.model.gameplay.DestinationCardHand
 import com.d.tickettoride.model.gameplay.TrainCarCardHand
 
-class User(var userName:String, var playerInfo:PlayerInfo?,
+class User(var username:String, var playerInfo:PlayerInfo?,
                 var trainCardHand: TrainCarCardHand?, var destinationHand:DestinationCardHand?) {
 
 
--- app/src/main/java/com/d/tickettoride/presenters/ChatPresenter.kt
+++ app/src/main/java/com/d/tickettoride/presenters/ChatPresenter.kt
@@ -5,19 +5,16 @@ import com.d.tickettoride.model.gameplay.Event
 import com.d.tickettoride.model.gameplay.EventType
 import com.d.tickettoride.presenters.ipresenters.IChatPresenter
 import com.d.tickettoride.service.ChatService
-import com.d.tickettoride.service.ChooseGameService
-import com.d.tickettoride.views.ChatFragment
 import com.d.tickettoride.views.iviews.IChatView
-import com.d.tickettoride.views.iviews.IChooseGameView
 
 class ChatPresenter(private val chatFragment: IChatView,
                     private val chatService: ChatService = ChatService.instance) : IChatPresenter {
 
     override fun sendMessage(message:String) {
-        val username = RootModel.instance.user!!.userName
+        val username = RootModel.instance.user!!.username
         val event = Event(EventType.MESSAGE, username, message)
 
-        ChatService.instance.sendChat(event)
+        chatService.sendChat(event)
     }
 
     override fun getChatList(): ArrayList<Event> {
