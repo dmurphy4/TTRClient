3bfa8d29ac7f98340a08dc1094c3394f76db0a63
dmurphy4 <murphy.dallin@byu.edu>
2019-03-07 14:04:36 -0700

    hi brad, this is dallin

--- app/src/main/java/com/d/tickettoride/model/gameplay/DestinationCard.kt
+++ app/src/main/java/com/d/tickettoride/model/gameplay/DestinationCard.kt
@@ -1,3 +1,3 @@
 package com.d.tickettoride.model.gameplay
 
-data class DestinationCard(val id:Int, val city1:City, val city2:City, val points:Int) : ICard
+data class DestinationCard(val id:Int, val city1:Int, val city2:Int, val points:Int) : ICard
--- app/src/main/java/com/d/tickettoride/presenters/ipresenters/IGamePresenter.kt
+++ app/src/main/java/com/d/tickettoride/presenters/ipresenters/IGamePresenter.kt
@@ -1,8 +1,9 @@
 package com.d.tickettoride.presenters.ipresenters
 
 import com.d.tickettoride.model.gameplay.Board
+import com.d.tickettoride.model.gameplay.City
 
 interface IGamePresenter {
     fun getBoard(): Board
-
+    fun getCityFromGame(id:Int) : City
 }
