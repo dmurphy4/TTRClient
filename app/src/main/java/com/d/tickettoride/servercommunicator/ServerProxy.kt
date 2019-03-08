bdd03697bb60e9b0dcd31e99202019983d54ef63
Brad <bradley.robert.hales@gmail.com>
2019-03-07 18:00:44 -0700

    another

--- app/src/main/java/com/d/tickettoride/servercommunicator/ServerProxy.kt
+++ app/src/main/java/com/d/tickettoride/servercommunicator/ServerProxy.kt
@@ -15,7 +15,7 @@ class ServerProxy {
         val client = OkHttpClient()
     }
 
-    private val url = "http://192.168.2.:8080/command"
+    private val url = "http://192.168.2.184:8080/command"
     private val JSON = MediaType.parse("application/json; charset=utf-8")
 
     fun command(command: ServerCommand) {
