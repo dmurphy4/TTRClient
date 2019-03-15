6a9060e41da32e2eb24bfbfa7d14337fc5e79fdc
dmurphy4 <murphy.dallin@byu.edu>
2019-03-15 14:44:43 -0600

    Finished changing stats

--- app/src/main/java/com/d/tickettoride/service/StatsService.kt
+++ app/src/main/java/com/d/tickettoride/service/StatsService.kt
@@ -10,21 +10,26 @@ class StatsService {
 
     fun addPoints(amount: Int, username: String) {
         RootModel.instance.game!!.getPlayerByUsername(username)!!.score += amount
+        RootModel.instance.game!!.statsChanged = true
     }
 
     fun addDestinations(amount: Int, username: String) {
         RootModel.instance.game!!.getPlayerByUsername(username)!!.numDestCards += amount
+        RootModel.instance.game!!.statsChanged = true
     }
 
     fun subtractTrainCars(amount: Int, username: String) {
         RootModel.instance.game!!.getPlayerByUsername(username)!!.numTrains -= amount
+        RootModel.instance.game!!.statsChanged = true
     }
 
     fun addTrainCarCards(amount: Int, username: String) {
         RootModel.instance.game!!.getPlayerByUsername(username)!!.numTrainCards += amount
+        RootModel.instance.game!!.statsChanged = true
     }
 
     fun decreaseTrainCarCards(amount: Int, username: String) {
         RootModel.instance.game!!.getPlayerByUsername(username)!!.numTrainCards -= amount
+        RootModel.instance.game!!.statsChanged = true
     }
 }
