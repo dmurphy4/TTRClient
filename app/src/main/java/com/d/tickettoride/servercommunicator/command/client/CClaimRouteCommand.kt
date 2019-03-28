53d176cfb7dd4fcc0c2dae1f6867323ae50ada6f
dmurphy4 <murphy.dallin@byu.edu>
2019-03-28 14:48:28 -0600

    asldkfjaslfkjd

--- app/src/main/java/com/d/tickettoride/servercommunicator/command/client/CClaimRouteCommand.kt
+++ app/src/main/java/com/d/tickettoride/servercommunicator/command/client/CClaimRouteCommand.kt
@@ -2,9 +2,9 @@ package com.d.tickettoride.servercommunicator.command.client
 
 import com.d.tickettoride.service.BoardService
 
-class CClaimRouteCommand(var id:Int, var username:String) : ICommand {
+class CClaimRouteCommand(var id:Int, var username:String, var errorMessage: String?) : ICommand {
 
     override fun execute() {
-        BoardService.instance.markRoute(id, username)
+        if (errorMessage == null) BoardService.instance.markRoute(id, username)
     }
 }
