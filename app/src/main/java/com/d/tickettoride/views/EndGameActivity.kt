d87e0b3574dc18d078827375abc507e15ea80fc7
zacharytaylor <zachtylr04@gmail.com>
2019-03-28 17:53:46 -0600

    Add end game layout and functionality

--- app/src/main/java/com/d/tickettoride/views/EndGameActivity.kt
+++ app/src/main/java/com/d/tickettoride/views/EndGameActivity.kt
@@ -6,19 +6,18 @@ import android.widget.Toast
 import com.d.tickettoride.presenters.EndGamePresenter
 import com.d.tickettoride.presenters.ipresenters.IEndGamePresenter
 import com.d.tickettoride.views.iviews.IEndGameView
+import kotlinx.android.synthetic.main.activity_end_game.*
 
 class EndGameActivity : AppCompatActivity(), IEndGameView {
 
     val endGamePresenter: IEndGamePresenter = EndGamePresenter(this)
-
-
 
     override fun displayErrorMessage(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 
     override fun displayWinner(username: String, points: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        end_game_title.text = "$username won with $points points!"
     }
 
     override fun displayPlayerStats(
@@ -30,10 +29,73 @@ class EndGameActivity : AppCompatActivity(), IEndGameView {
         mostRoutePoints: Int,
         totalPoints: Int
     ) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when(row) {
+            1 -> displayRow1(username, claimedPts, destinationPts, negDestinationPts, mostRoutePoints, totalPoints)
+            2 -> displayRow2(username, claimedPts, destinationPts, negDestinationPts, mostRoutePoints, totalPoints)
+            3 -> displayRow3(username, claimedPts, destinationPts, negDestinationPts, mostRoutePoints, totalPoints)
+            4 -> displayRow4(username, claimedPts, destinationPts, negDestinationPts, mostRoutePoints, totalPoints)
+            5 -> displayRow5(username, claimedPts, destinationPts, negDestinationPts, mostRoutePoints, totalPoints)
+        }
     }
 
     override fun goBackToChooseGameActivity() {
-        startActivity(Intent(this, ChooseGameActivity::class.java))
+        val intent = Intent(this, ChooseGameActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+    }
+
+    private fun displayRow1(username: String, claimedPts: Int, destinationPts: Int, negDestinationPts: Int,
+                            mostRoutePoints: Int, totalPoints: Int) {
+        place_1.text = "1st"
+        username_1.text = username
+        claimed_1.text = claimedPts.toString()
+        destination_1.text = destinationPts.toString()
+        lost_destination_1.text = negDestinationPts.toString()
+        most_routes_1.text = mostRoutePoints.toString()
+        total_1.text = totalPoints.toString()
+    }
+
+    private fun displayRow2(username: String, claimedPts: Int, destinationPts: Int, negDestinationPts: Int,
+                            mostRoutePoints: Int, totalPoints: Int) {
+        place_2.text = "2nd"
+        username_2.text = username
+        claimed_2.text = claimedPts.toString()
+        destination_2.text = destinationPts.toString()
+        lost_destination_2.text = negDestinationPts.toString()
+        most_routes_2.text = mostRoutePoints.toString()
+        total_2.text = totalPoints.toString()
+    }
+
+    private fun displayRow3(username: String, claimedPts: Int, destinationPts: Int, negDestinationPts: Int,
+                            mostRoutePoints: Int, totalPoints: Int) {
+        place_3.text = "3rd"
+        username_3.text = username
+        claimed_3.text = claimedPts.toString()
+        destination_3.text = destinationPts.toString()
+        lost_destination_3.text = negDestinationPts.toString()
+        most_routes_3.text = mostRoutePoints.toString()
+        total_3.text = totalPoints.toString()
+    }
+
+    private fun displayRow4(username: String, claimedPts: Int, destinationPts: Int, negDestinationPts: Int,
+                            mostRoutePoints: Int, totalPoints: Int) {
+        place_4.text = "4th"
+        username_4.text = username
+        claimed_4.text = claimedPts.toString()
+        destination_4.text = destinationPts.toString()
+        lost_destination_4.text = negDestinationPts.toString()
+        most_routes_4.text = mostRoutePoints.toString()
+        total_4.text = totalPoints.toString()
+    }
+
+    private fun displayRow5(username: String, claimedPts: Int, destinationPts: Int, negDestinationPts: Int,
+                            mostRoutePoints: Int, totalPoints: Int) {
+        place_5.text = "5th"
+        username_5.text = username
+        claimed_5.text = claimedPts.toString()
+        destination_5.text = destinationPts.toString()
+        lost_destination_5.text = negDestinationPts.toString()
+        most_routes_5.text = mostRoutePoints.toString()
+        total_5.text = totalPoints.toString()
     }
 }
