02b0832fa77afaaa9f13810b6fb9b58c5aa99fc7
dmurphy4 <murphy.dallin@byu.edu>
2019-04-01 11:26:36 -0600

    alsdfj

--- app/src/main/java/com/d/tickettoride/servercommunicator/command/client/CEndGameCommand.kt
+++ app/src/main/java/com/d/tickettoride/servercommunicator/command/client/CEndGameCommand.kt
@@ -3,9 +3,10 @@ package com.d.tickettoride.servercommunicator.command.client
 import com.d.tickettoride.model.GameSummary
 import com.d.tickettoride.service.EndGameService
 
-class CEndGameCommand(val gameSummary: GameSummary, val winner: String, val winnerPoints: Int) : ICommand {
+class CEndGameCommand(val summary: GameSummary, val winner: String, val winnerPoints: Int) : ICommand {
 
     override fun execute() {
-        EndGameService.instance.endGame(gameSummary, winner, winnerPoints)
+        println("$winner is the winner of the game! With ")
+        EndGameService.instance.endGame(summary, winner, winnerPoints)
     }
 }
