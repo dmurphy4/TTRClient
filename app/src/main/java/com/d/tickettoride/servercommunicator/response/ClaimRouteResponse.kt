d4650a5b2975f4c4e12d4601de8689f7a63b43de
dmurphy4 <murphy.dallin@byu.edu>
2019-04-17 10:16:02 -0600

    hi

--- app/src/main/java/com/d/tickettoride/servercommunicator/response/ClaimRouteResponse.kt
+++ app/src/main/java/com/d/tickettoride/servercommunicator/response/ClaimRouteResponse.kt
@@ -1,10 +1,16 @@
 package com.d.tickettoride.servercommunicator.response
 
 import com.d.tickettoride.service.BoardService
+import com.d.tickettoride.service.ErrorMessageService
 
 class ClaimRouteResponse(val id: Int) : GenericResponse() {
 
     override fun execute() {
-        BoardService.instance.decreaseCardsPostClaim(id, null) // null because it's not gray
+        if (errorMessage == null) {
+            BoardService.instance.decreaseCardsPostClaim(id, null) // null because it's not gray
+        }
+        else {
+            ErrorMessageService.instance.postErrorMessage(errorMessage)
+        }
     }
 }
